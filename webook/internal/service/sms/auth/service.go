2dcf32b49fb9f0e66fc9498a4d1a37de6aca0c36
liupch6 <liupch6@gmail.com>
2024-11-26 21:37:31 +0800

    可重试的短信服务,token 实现短信的权限控制

---
+++ webook/internal/service/sms/auth/service.go
@@ -0,0 +1,40 @@
+package auth
+
+import (
+	"context"
+	"errors"
+
+	"github.com/golang-jwt/jwt/v5"
+
+	"basic-go/webook/internal/service/sms"
+)
+
+type Service struct {
+	svc sms.Service
+	key []byte
+}
+
+func NewService(svc sms.Service, key []byte) sms.Service {
+	return &Service{svc: svc, key: key}
+}
+
+type Claims struct {
+	jwt.RegisteredClaims
+	tplId string
+}
+
+// Send 使用 token 进行短信服务的权限控制,这里 tplToken 的语义发生了改变,之前就是 tplId, 这里是带 token 的
+// 这个 tplToken 是线下申请的代表业务方的 token
+func (s *Service) Send(ctx context.Context, tplToken string, params []string, numbers ...string) error {
+	var c Claims
+	token, err := jwt.ParseWithClaims(tplToken, &c, func(token *jwt.Token) (interface{}, error) {
+		return s.key, nil
+	})
+	if err != nil {
+		return err
+	}
+	if !token.Valid {
+		return errors.New("短信服务 token 不合法")
+	}
+	return s.svc.Send(ctx, tplToken, params, numbers...)
+}
---
+++ webook/internal/service/sms/retryable/service.go
@@ -0,0 +1,34 @@
+package retryable
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"basic-go/webook/internal/service/sms"
+)
+
+type Service struct {
+	svc      sms.Service
+	retryMax int
+}
+
+func (s *Service) Send(ctx context.Context, tplId string, params []string, numbers ...string) error {
+	var err error
+	for i := 0; i < s.retryMax; i++ {
+		err = s.svc.Send(ctx, tplId, params, numbers...)
+		if err == nil {
+			return nil
+		}
+		// 稍微增加延迟,不要立马重试
+		select {
+		case <-ctx.Done():
+			log.Println("Context cancelled, aborting retries")
+			return ctx.Err()
+		case <-time.After(time.Second):
+			log.Println("Retrying after 1 second")
+		}
+	}
+	return fmt.Errorf("短信发送重试都失败了: %w", err)
+}
